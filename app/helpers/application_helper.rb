8566a6a6064e44bc1ac7ffaebaef21e7fc91b766
Daniel <danielallendeutsch@gmail.com>
2016-05-01 16:43:28 -0400

    moves forms for book and page into partial to reduce code duplication.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def profile_page?
     action_name == 'index' && controller_name == 'books'
   end
+
+  def destroy_book_confirmation
+    'Are you sure you want to delete this book? All the pages will be lost.'
+  end
+
+  def destroy_page_confirmation
+    'Are you sure you want to delete this page?'
+  end
 end
